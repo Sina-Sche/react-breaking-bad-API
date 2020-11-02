2f5874ddd4cbcfe4abdffe656d9a6012094df458
Sina-Sche <si.scheckenbach@gmail.com>
2020-11-02 19:42:09 +0100

    ✨Add Basic Layout and functionality

--- src/App.js
+++ src/App.js
@@ -4,34 +4,33 @@ import Header from "./components/Header";
 // import CharacterGrid from "./components/characters/CharacterGrid";
 // import Search from "./components/ui/Search";
 import "./App.css";
+import CharacterGrid from "./components/CharacterGrid";
+import Search from "./components/Search";
 
 const App = () => {
   const [items, setItems] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [query, setQuery] = useState(true);
   // const [query, setQuery] = useState("");
 
-  useEffect(
-    () => {
-      const fetchItems = async () => {
-        setIsLoading(true);
-        const result = await axios(
-          `https://www.breakingbadapi.com/api/characters`
-        );
+  useEffect(() => {
+    const fetchItems = async () => {
+      setIsLoading(true);
+      const result = await axios(
+        `https://www.breakingbadapi.com/api/characters?name=${query}`
+      );
 
-        // setItems(result.data);
-        // setIsLoading(false);
+      setItems(result.data);
+      setIsLoading(false);
+    };
+    fetchItems();
+  }, [query]);
 
-        console.log(result.data);
-      };
-      fetchItems();
-    }
-    // }, [query]);
-  );
   return (
     <div className="container">
       <Header />
-      {/* <Search getQuery={(q) => setQuery(q)} />
-      <CharacterGrid isLoading={isLoading} items={items} /> */}
+      <Search getQuery={(query) => setQuery(query)} />
+      <CharacterGrid isLoading={isLoading} items={items} />
     </div>
   );
 };
---
+++ src/components/Search.js
@@ -0,0 +1,26 @@
+import { getSuggestedQuery } from "@testing-library/react";
+import React, { useState } from "react";
+
+const Search = ({ getQuery }) => {
+  const [text, setText] = useState("");
+  const onChange = (query) => {
+    setText(query);
+    getQuery(query);
+  };
+  return (
+    <section className="search">
+      <form>
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Search characters"
+          value={text}
+          onChange={(e) => onChange(e.target.value)}
+          autoFocus=""
+        />
+      </form>
+    </section>
+  );
+};
+
+export default Search;
